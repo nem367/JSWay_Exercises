933895ee3ae4f662311829c97eadf763851662e4
nem367 <104479991+nem367@users.noreply.github.com>
2022-11-15 15:56:09 -0500

    ugh

--- rick&morty/script.js
+++ rick&morty/script.js
@@ -1,19 +1,9 @@
 var name;
 var image;
 var status;
-var counter = 0;
+var counter=0;
 
 document.getElementById("character").addEventListener("click", display);
-
-// let randomNumber = Math.floor(Math.random() * 826);
-// fetch("https://rickandmortyapi.com/api/character/"+randomNumber)
-// .then((response) => response.json())
-// .then((data) => {
-//   name = data.name;
-//   image = data.image;
-//   status = data.status;
-//   console.log(name, status);
-// });
 
 function display(){
   let randomNumber = Math.floor(Math.random() * 826);
@@ -23,9 +13,9 @@ function display(){
     name = data.name;
     image = data.image;
     status = data.status;
-    console.log(name, status);
-    document.getElementById("characterName").innerHTML = `<p>${name}</p>`;
-    document.getElementById("characterName").innerHTML = `<img src="${image}">`;
+    // console.log(name, status);
+    document.getElementById("characterName").innerHTML = `<p>This character's name is: ${name}</p>`;
+    document.getElementById("characterImage").innerHTML = `<img src="${image}">`;
     document.body.style.backgroundColor = "white";
   });
 }
@@ -35,22 +25,24 @@ document.getElementById("dead").addEventListener("click", deadChecker);
 function deadChecker(){
     if(status==="Dead"){
       document.body.style.backgroundColor = "green";
-      counter+=1
+      counter++;
      }else{
-       document.body.style.backgroundColor = "red";
-       counter-=1
+      document.body.style.backgroundColor = "red";
+      counter--;
      }
 }
 
-document.getElementById("dead").addEventListener("click", aliveChecker);
+document.getElementById("alive").addEventListener("click", aliveChecker);
 
 function aliveChecker(){
   if(status==="Alive"){
     document.body.style.backgroundColor = "green";
-    counter+=1
+    // counter+=1;
+    counter++;
   }else{
     document.body.style.backgroundColor = "red";
-    counter-=1
+    // counter-=1;
+    counter--;
   }
 }
 
@@ -59,13 +51,15 @@ document.getElementById("unknown").addEventListener("click", unknownChecker);
 function unknownChecker(){
     if(status==="Unknown"){
     document.body.style.backgroundColor = "green";
-      counter+=1
+    counter++;
   }else{
     document.body.style.backgroundColor = "red";
-    counter-=1
+    counter--;
   }
 }
 
 console.log(counter);
 //we need to fix the counter
 //need to fix david's bug of not being able to submit another response
+
+document.getElementById('score').innerText = `Score: ${counter}`;
